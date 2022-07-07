50b5250f93d4b40635149b27e7807de9f48f8983
xhhsc <xhhsc@outlook.com>
2022-07-07 19:40:45 +0800

    mybatis

---
+++ demo-spring-web/src/main/java/cn/byteboy/demo/spring/web/mybatis/MyInterceptor.java
@@ -0,0 +1,33 @@
+package cn.byteboy.demo.spring.web.mybatis;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author hongshaochuan
+ */
+@Slf4j
+@Component
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = {MappedStatement.class, Object.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class}),
+        @Signature(type = Executor.class, method = "query", args = {MappedStatement.class, Object.class, RowBounds.class, ResultHandler.class, CacheKey.class, BoundSql.class})
+})
+public class MyInterceptor implements Interceptor {
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        log.info("MyInterceptor exec, method: {}",  invocation.getMethod().getName());
+        return invocation.proceed();
+    }
+}
